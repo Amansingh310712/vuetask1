dc059a7b98d94f33c848cb3b6dd59be0563282b8
Aman singh <viknesh.vsm@gmail.com>
2025-01-02 18:00:31 +0530

    mobile responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
-    <Navbar/>
-    <Scale/>
-    <Highlightingpart/>
-    <!-- <Review/> -->
-    <Helping/>
-    <!-- <Footer/> -->
+    <Navbar />
+    <Scale />
+    <Highlightingpart />
+    <Review />
+    <Helping />
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Navbar from '~/components/Navbar.vue';
 import Highlightingpart from '~/components/Highlightingpart.vue';
-// import Footer from '~/components/Footer.vue'
+import Footer from '~/components/Footer.vue'
 import Scale from '~/components/Scale.vue'
 import Helping from '~/components/Helping.vue'
-// import Review from '~/components/Review.vue'
+import Review from '~/components/Review.vue'
 useHead({
   title: "Pinwheel-tailwind"
 })
 </script>
 
-<style>
-
-
-</style>
-  
+<style></style>
